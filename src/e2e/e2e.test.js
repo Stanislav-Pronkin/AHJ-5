91e406175d9446a0edb12aa30b311f1a11d478dd
Stanislav Pronkin <stanley_@mail.ru>
2021-11-05 21:59:21 +0700

    Update e2e.test

--- src/e2e/e2e.test.js
+++ src/e2e/e2e.test.js
@@ -3,48 +3,48 @@ import { fork } from 'child_process';
 
 jest.setTimeout(30000); // default puppeteer timeout
 
-describe('Credit Card Validator form', () => {
-  let browser = null;
-  let page = null;
-  let server = null;
-  const baseUrl = 'http://localhost:9000';
+describe('Test popup', () => {
+    let browser = null;
+    let page = null;
+    let server = null;
+    const baseUrl = 'http://localhost:9000';
 
-  beforeAll(async () => {
-    server = fork(`${__dirname}/e2e.server.js`);
-    await new Promise((resolve, reject) => {
-      server.on('error', reject);
-      server.on('message', (message) => {
-        if (message === 'ok') {
-          resolve();
-        }
-      });
+    beforeAll(async() => {
+        server = fork(`${__dirname}/e2e.server.js`);
+        await new Promise((resolve, reject) => {
+            server.on('error', reject);
+            server.on('message', (message) => {
+                if (message === 'ok') {
+                    resolve();
+                }
+            });
+        });
+
+        browser = await puppetteer.launch({
+            headless: false, // show gui
+            slowMo: 250,
+            devtools: true, // show devTools
+        });
+        page = await browser.newPage();
     });
 
-    browser = await puppetteer.launch({
-      headless: false, // show gui
-      slowMo: 250,
-      devtools: true, // show devTools
-    });
-    page = await browser.newPage();
-  });
-
-  afterAll(async () => {
-    await browser.close();
-    server.kill();
-  });
-
-  describe('Test click', () => {
-    test('Show popup', async () => {
-      await page.goto(baseUrl);
-      const button = await page.$('[class=btn]');
-      button.click();
-      await page.waitFor(() => document.querySelector('.popup'));
+    afterAll(async() => {
+        await browser.close();
+        server.kill();
     });
 
-    test('Close popup', async () => {
-      const button = await page.$('[class=btn]');
-      button.click();
-      await page.waitFor(() => !document.querySelector('.popup'));
+    describe('Test click', () => {
+        test('Show popup', async() => {
+            await page.goto(baseUrl);
+            const button = await page.$('[class=btn]');
+            button.click();
+            await page.waitFor(() => document.querySelector('.popup'));
+        });
+
+        test('Close popup', async() => {
+            const button = await page.$('[class=btn]');
+            button.click();
+            await page.waitFor(() => !document.querySelector('.popup'));
+        });
     });
-  });
 });
